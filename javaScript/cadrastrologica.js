3693c15bd661ed67d23e2b0928556324592d4c9c
Elena-Mimouni <elenamimouni@gmail.com>
2025-09-12 17:27:08 -0300

    commit inicial

--- javaScript/cadrastrologica.js
+++ javaScript/cadrastrologica.js
@@ -2,11 +2,12 @@
 let usuarios = [{"nome":"Elena","email":"elena@gmail.com","senha":"teste123"}]
 localStorage.setItem("usuarios",JSON.stringify(usuarios));
 
+
 function cadastrar(){
     // pega dados escritos nos campos
-    let nome = document.getElementById("nome").value;
-    let email = document.getElementById("email").value;
-    let senha = document.getElementById("senha").value;
+    let nome = document.getElementById("nomeCadastro").value;
+    let email = document.getElementById("emailCadastro").value;
+    let senha = document.getElementById("senhaCadastro").value;
 
 
     // verifica se algum está vazio
@@ -37,5 +38,42 @@ function cadastrar(){
     document.getElementById("nome").value="";
     document.getElementById("email").value="";
     document.getElementById("senha").value="";
+
+}
+
+function login(){
+    // pega dados escritos nos campos
+    let email = document.getElementById("emailLogin").value;
+    let senha = document.getElementById("senhaLogin").value;
+
+    // verifica se algum está vazio
+    if (!email||!senha){
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    // pega a lista de usuarios existentes
+    let usuarios = JSON.parse(localStorage.getItem("usuarios"));
+
+
+    // verifica se usuario está no banco de dados
+    const existe = usuarios.find(user => user.email === email);
+
+    // se o usuario já existe, fecha a função
+    if (!existe){
+        alert("Usuário não encontrado, verifique se as informações foram inseridas corretamente ou faça seu cadastro");
+        return;
+    }
+
+    const verificaInformações = usuarios.find(user => user.email === email && user.senha === senha);
+
+    if (verificaInformações){
+        alert("Bem-vindo, ${usuario.nome}!")
+    }
+    else{
+        alert("Senha errada, tente novamente")
+    }
+
+    
 
 }
