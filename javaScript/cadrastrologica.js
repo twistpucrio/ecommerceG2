79154bb50626ed3c99412e11c4e8e3e3ffb614ac
Elena-Mimouni <elenamimouni@gmail.com>
2025-09-15 17:33:00 -0300

    commit final branch

--- javaScript/cadrastrologica.js
+++ javaScript/cadrastrologica.js
@@ -42,6 +42,14 @@ function cadastrar(){
 }
 
 function login(){
+    // verifica que não há nenhum login já efetuado para não termos duas contas logadas
+    const verificaLoginExistente = JSON.parse(localStorage.getItem("usuarioLogado"));
+
+    if (verificaLoginExistente){
+        alert("Faça logout da conta atual antes de prosseguir");
+        return;
+    }
+
     // pega dados escritos nos campos
     let email = document.getElementById("emailLogin").value;
     let senha = document.getElementById("senhaLogin").value;
@@ -57,23 +65,42 @@ function login(){
 
 
     // verifica se usuario está no banco de dados
-    const existe = usuarios.find(user => user.email === email);
+    const usuario = usuarios.find(user => user.email === email);
 
     // se o usuario já existe, fecha a função
-    if (!existe){
+    if (!usuario){
         alert("Usuário não encontrado, verifique se as informações foram inseridas corretamente ou faça seu cadastro");
         return;
     }
 
+    // olha no banco de dados dos cadastros para tentar dar match nos emails e senhas cadastradas
     const verificaInformações = usuarios.find(user => user.email === email && user.senha === senha);
 
+    // se houver um match, faz um alerta e adiciona um JSON com os dados do usuario, esse JSON significa que um login foi efetuado
     if (verificaInformações){
-        alert("Bem-vindo, ${usuario.nome}!")
+        alert("Boas vindas, "+ usuario.nome +"!");
+        localStorage.setItem("usuarioLogado",JSON.stringify(usuario));
     }
     else{
-        alert("Senha errada, tente novamente")
+        alert("Senha errada, tente novamente");
     }
 
     
 
+}
+
+function logout(){
+    //procura se há um usuario com login feito atualmente, se não houver "usarioLogado" no localStorage então não há nenhum login efetuado no momento
+    const usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado"));
+
+
+    // se for falso, nada além de um alerta acontece
+    if (!usuarioLogado){
+        alert("Nenhum usuário logado atualmente");
+    }
+    // se houver um usuário, exibe uma mensagem e remove o JSON do login do localStorage
+    else{
+        alert(usuarioLogado.nome + ", você saiu da sua conta!");
+        localStorage.removeItem("usuarioLogado");
+    }
 }
